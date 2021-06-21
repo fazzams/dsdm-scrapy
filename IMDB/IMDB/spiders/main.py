1adaf7ce0b93acb5dcd56472a09f8477bf92934d
Azzam <varokahdotnet@gmail.com>
2021-06-21 20:54:25 +0800

    End of Scrapy IMDB Lesson

--- IMDB/IMDB/spiders/main.py
+++ IMDB/IMDB/spiders/main.py
@@ -4,33 +4,47 @@ class SctrQuotes(scrapy.Spider):
     name = 'imdb'
     start_urls = ['https://www.imdb.com/chart/top/']
 
-
     def parse(self, response):
         for i in response.css('.titleColumn a'):
             movieName = i.css('::text').get()
             url = i.css('::attr(href)').get()
             dic = {
-                'movie': movieName,
+                'Movie': movieName,
             }
-            yield response.follow(url, callback=self.parseInfo, meta=dic)
+            yield response.follow(url, callback=self.parseMovie, meta=dic)
 
 
-
-
-    def parseInfo(self, response):
+    def parseMovie(self, response):
             print('\n\n\n\n')
-            movieName = response.meta['movie']
+            movieName = response.meta['Movie']
             duration = response.css('.subtext time::text').get().strip()
             genre = ', '.join(response.css('.subtext a:not(:last-child)::text').getall())
 
+
+            directorName = response.css('h4:contains("Director") + a::text').get()
+            directorUrl = response.css('h4:contains("Director") + a::attr(href)').get()
+
             # print(movieName, duration, genre)
 
-            yield {
+            dic = {
                 'Movie Name': movieName,
                 'Duration': duration,
-                'Genre': genre
+                'Genre': genre,
+                'Director Name': directorName
             }
 
+            yield response.follow(directorUrl, callback=self.parseDir, meta=dic, dont_filter= True)
+
+    def parseDir(self, response):
+        topFourMovies = response.css('.knownfor-title-role a::text').getall()
+        topFourMovies = ', '.join(topFourMovies)
+        yield {
+            'Movie Name': response.meta['Movie Name'],
+            'Duration': response.meta['Duration'],
+            'Genre': response.meta['Genre'],
+            'Director Name': response.meta['Director Name'],
+            'Top For Movies': topFourMovies
+        }
 
 
 
